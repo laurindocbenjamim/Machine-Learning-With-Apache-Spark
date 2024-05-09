e6fd34966a317cb23ca78533539afdd359612eb5
adolfosucumula <rocketmc2009@gmail.com>
2024-05-09 13:38:52 +0100

    Random row, shaping and showing rows and columns and scatter plot

--- project_01.py
+++ project_01.py
@@ -2,3 +2,24 @@
 def warn(*args, **kwargs):
     pass
 import warnings
+warnings.warn = warn
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# the data set is available at the url below.
+URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-BD0231EN-SkillsNetwork/datasets/mpg.csv"
+
+# using the read_csv function in the pandas library, we load the data into a dataframe.
+
+df = pd.read_csv(URL)
+
+# show 5 random rows from the dataset
+df.sample(5)
+
+# Find  out the number of  rows and columns
+df.shape
+
+# create a scatter plot of Horsepower versus mileage(MPG) . This will help us visualize the relationship between them.
+df.plot.scatter(x = "Horsepower", y = "MPG")
